3831ca17d82b5b8eccdd7edfac573dd65fef983f
Frederik Vannoote <frederikvannoote@users.noreply.github.com>
2025-10-18 21:43:54 +0200

    New translations gsync_pt-pt.ts (Hindi)

---
+++ hi/GSync_pt-PT.ts
@@ -0,0 +1,67 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="hi-IN" sourcelanguage="en">
+  <context>
+    <name>SyncWindow</name>
+    <message>
+      <location filename="syncwindow.ui" line="14"/>
+      <source>SyncWindow</source>
+      <translation type="unfinished">SyncWindow</translation>
+    </message>
+    <message>
+      <location filename="syncwindow.ui" line="43"/>
+      <source>Connect to your Google Drive</source>
+      <translation type="unfinished">Connect to your Google Drive</translation>
+    </message>
+    <message>
+      <location filename="syncwindow.ui" line="66"/>
+      <source>Connect</source>
+      <translation type="unfinished">Connect</translation>
+    </message>
+    <message>
+      <location filename="syncwindow.ui" line="89"/>
+      <source>Connected</source>
+      <translation type="unfinished">Connected</translation>
+    </message>
+    <message>
+      <location filename="syncwindow.ui" line="131"/>
+      <source>Synchronize to:</source>
+      <translation type="unfinished">Synchronize to:</translation>
+    </message>
+    <message>
+      <location filename="syncwindow.ui" line="144"/>
+      <source>...</source>
+      <translation type="unfinished">...</translation>
+    </message>
+    <message>
+      <location filename="syncwindow.ui" line="183"/>
+      <source>Start</source>
+      <translation type="unfinished">Start</translation>
+    </message>
+    <message>
+      <location filename="syncwindow.ui" line="213"/>
+      <source>Discovering Google Drive</source>
+      <translation type="unfinished">Discovering Google Drive</translation>
+    </message>
+    <message>
+      <location filename="syncwindow.ui" line="271"/>
+      <source>Found 0 files</source>
+      <translation type="unfinished">Found 0 files</translation>
+    </message>
+    <message>
+      <location filename="syncwindow.ui" line="297"/>
+      <source>Cancel</source>
+      <translation type="unfinished">Cancel</translation>
+    </message>
+    <message>
+      <location filename="syncwindow.cpp" line="40"/>
+      <source>Found %1 files</source>
+      <translation type="unfinished">Found %1 files</translation>
+    </message>
+    <message>
+      <location filename="syncwindow.cpp" line="50"/>
+      <source>Select target directory</source>
+      <translation type="unfinished">Select target directory</translation>
+    </message>
+  </context>
+</TS>
