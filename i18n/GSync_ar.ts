71112dc98490630aa1d6e6a1526bca9d1940dd3d
Frederik Vannoote <frederikvannoote@users.noreply.github.com>
2025-10-19 16:45:18 +0200

    New translations gsync_en.ts (Arabic)

--- i18n/GSync_ar.ts
+++ i18n/GSync_ar.ts
@@ -5,8 +5,8 @@
     <name>SyncWindow</name>
     <message>
       <location filename="../syncwindow.ui" line="14"/>
-      <source>SyncWindow</source>
-      <translation>مزامنة نافذة</translation>
+      <source>Synchronize your Google Drive</source>
+      <translation type="unfinished">Synchronize your Google Drive</translation>
     </message>
     <message>
       <location filename="../syncwindow.ui" line="43"/>
