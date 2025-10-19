a9d082b6dadb9a130cd8be2a29de28aec8085668
Frederik Vannoote <frederikvannoote@users.noreply.github.com>
2025-10-19 16:30:45 +0200

    New translations gsync_en.ts (Arabic)

---
+++ i18n/GSync_ar.ts
@@ -0,0 +1,67 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ar" sourcelanguage="en">
+  <context>
+    <name>SyncWindow</name>
+    <message>
+      <location filename="../syncwindow.ui" line="14"/>
+      <source>SyncWindow</source>
+      <translation>مزامنة نافذة</translation>
+    </message>
+    <message>
+      <location filename="../syncwindow.ui" line="43"/>
+      <source>Connect to your Google Drive</source>
+      <translation>اتصل بـ Google Drive</translation>
+    </message>
+    <message>
+      <location filename="../syncwindow.ui" line="66"/>
+      <source>Connect</source>
+      <translation>الاتصال</translation>
+    </message>
+    <message>
+      <location filename="../syncwindow.ui" line="89"/>
+      <source>Connected</source>
+      <translation>متصل</translation>
+    </message>
+    <message>
+      <location filename="../syncwindow.ui" line="131"/>
+      <source>Synchronize to:</source>
+      <translation>مزامنة إلى:</translation>
+    </message>
+    <message>
+      <location filename="../syncwindow.ui" line="144"/>
+      <source>...</source>
+      <translation>...</translation>
+    </message>
+    <message>
+      <location filename="../syncwindow.ui" line="183"/>
+      <source>Start</source>
+      <translation>ابدأ</translation>
+    </message>
+    <message>
+      <location filename="../syncwindow.ui" line="213"/>
+      <source>Discovering Google Drive</source>
+      <translation>اكتشاف Google Drive</translation>
+    </message>
+    <message>
+      <location filename="../syncwindow.ui" line="271"/>
+      <source>Found 0 files</source>
+      <translation>تم العثور على ملفات </translation>
+    </message>
+    <message>
+      <location filename="../syncwindow.ui" line="297"/>
+      <source>Cancel</source>
+      <translation>إلغاء</translation>
+    </message>
+    <message>
+      <location filename="../syncwindow.cpp" line="40"/>
+      <source>Found %1 files</source>
+      <translation>تم العثور على ملفات 1%</translation>
+    </message>
+    <message>
+      <location filename="../syncwindow.cpp" line="50"/>
+      <source>Select target directory</source>
+      <translation>اختيار الدليل الهدف</translation>
+    </message>
+  </context>
+</TS>
