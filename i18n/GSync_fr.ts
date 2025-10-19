dd20654692ee0144d01fd628e8b7b0cea4c70f01
Frederik Vannoote <frederikvannoote@users.noreply.github.com>
2025-10-19 16:30:44 +0200

    New translations gsync_en.ts (French)

---
+++ i18n/GSync_fr.ts
@@ -0,0 +1,67 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr" sourcelanguage="en">
+  <context>
+    <name>SyncWindow</name>
+    <message>
+      <location filename="../syncwindow.ui" line="14"/>
+      <source>SyncWindow</source>
+      <translation>Synchroniser la fenêtre</translation>
+    </message>
+    <message>
+      <location filename="../syncwindow.ui" line="43"/>
+      <source>Connect to your Google Drive</source>
+      <translation>Connectez-vous à votre Google Drive</translation>
+    </message>
+    <message>
+      <location filename="../syncwindow.ui" line="66"/>
+      <source>Connect</source>
+      <translation>Connecter</translation>
+    </message>
+    <message>
+      <location filename="../syncwindow.ui" line="89"/>
+      <source>Connected</source>
+      <translation>Connecté</translation>
+    </message>
+    <message>
+      <location filename="../syncwindow.ui" line="131"/>
+      <source>Synchronize to:</source>
+      <translation>Synchroniser vers:</translation>
+    </message>
+    <message>
+      <location filename="../syncwindow.ui" line="144"/>
+      <source>...</source>
+      <translation>...</translation>
+    </message>
+    <message>
+      <location filename="../syncwindow.ui" line="183"/>
+      <source>Start</source>
+      <translation>Début</translation>
+    </message>
+    <message>
+      <location filename="../syncwindow.ui" line="213"/>
+      <source>Discovering Google Drive</source>
+      <translation>Découvrir Google Drive</translation>
+    </message>
+    <message>
+      <location filename="../syncwindow.ui" line="271"/>
+      <source>Found 0 files</source>
+      <translation>0 fichier trouvé</translation>
+    </message>
+    <message>
+      <location filename="../syncwindow.ui" line="297"/>
+      <source>Cancel</source>
+      <translation>Abandonner</translation>
+    </message>
+    <message>
+      <location filename="../syncwindow.cpp" line="40"/>
+      <source>Found %1 files</source>
+      <translation>Fichiers %1 trouvés</translation>
+    </message>
+    <message>
+      <location filename="../syncwindow.cpp" line="50"/>
+      <source>Select target directory</source>
+      <translation>Sélectionnez le répertoire cible</translation>
+    </message>
+  </context>
+</TS>
