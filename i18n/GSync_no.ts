aa48903d96b7cef7a5e52455d1392d063da7bc6d
Frederik Vannoote <frederikvannoote@users.noreply.github.com>
2025-10-19 16:30:51 +0200

    New translations gsync_en.ts (Norwegian)

---
+++ i18n/GSync_no.ts
@@ -0,0 +1,67 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="no" sourcelanguage="en">
+  <context>
+    <name>SyncWindow</name>
+    <message>
+      <location filename="../syncwindow.ui" line="14"/>
+      <source>SyncWindow</source>
+      <translation>SynkVindu</translation>
+    </message>
+    <message>
+      <location filename="../syncwindow.ui" line="43"/>
+      <source>Connect to your Google Drive</source>
+      <translation>Koble til din Google Drive</translation>
+    </message>
+    <message>
+      <location filename="../syncwindow.ui" line="66"/>
+      <source>Connect</source>
+      <translation>Nettverk</translation>
+    </message>
+    <message>
+      <location filename="../syncwindow.ui" line="89"/>
+      <source>Connected</source>
+      <translation>Tilkoblet</translation>
+    </message>
+    <message>
+      <location filename="../syncwindow.ui" line="131"/>
+      <source>Synchronize to:</source>
+      <translation>Synkroniser til:</translation>
+    </message>
+    <message>
+      <location filename="../syncwindow.ui" line="144"/>
+      <source>...</source>
+      <translation>...</translation>
+    </message>
+    <message>
+      <location filename="../syncwindow.ui" line="183"/>
+      <source>Start</source>
+      <translation>Begynn</translation>
+    </message>
+    <message>
+      <location filename="../syncwindow.ui" line="213"/>
+      <source>Discovering Google Drive</source>
+      <translation>Oppdager Google Drive</translation>
+    </message>
+    <message>
+      <location filename="../syncwindow.ui" line="271"/>
+      <source>Found 0 files</source>
+      <translation>Fant 0 filer</translation>
+    </message>
+    <message>
+      <location filename="../syncwindow.ui" line="297"/>
+      <source>Cancel</source>
+      <translation>Avbryt</translation>
+    </message>
+    <message>
+      <location filename="../syncwindow.cpp" line="40"/>
+      <source>Found %1 files</source>
+      <translation>Fant %1 filer</translation>
+    </message>
+    <message>
+      <location filename="../syncwindow.cpp" line="50"/>
+      <source>Select target directory</source>
+      <translation>Velg målmappen:</translation>
+    </message>
+  </context>
+</TS>
