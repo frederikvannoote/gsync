f5f3288999290b3d27fa0c1da5d81de1bf58adae
Frederik Vannoote <frederikvannoote@users.noreply.github.com>
2025-10-19 15:37:13 +0200

    New translations gsync_en.ts (Danish)

--- i18n/GSync_da.ts
+++ i18n/GSync_da.ts
@@ -6,62 +6,62 @@
     <message>
       <location filename="../syncwindow.ui" line="14"/>
       <source>SyncWindow</source>
-      <translation type="unfinished">SyncWindow</translation>
+      <translation>Synkroniseringsvindue</translation>
     </message>
     <message>
       <location filename="../syncwindow.ui" line="43"/>
       <source>Connect to your Google Drive</source>
-      <translation type="unfinished">Connect to your Google Drive</translation>
+      <translation>Opret forbindelse til dit Google Drev</translation>
     </message>
     <message>
       <location filename="../syncwindow.ui" line="66"/>
       <source>Connect</source>
-      <translation type="unfinished">Connect</translation>
+      <translation>Forbind</translation>
     </message>
     <message>
       <location filename="../syncwindow.ui" line="89"/>
       <source>Connected</source>
-      <translation type="unfinished">Connected</translation>
+      <translation>Forbundet</translation>
     </message>
     <message>
       <location filename="../syncwindow.ui" line="131"/>
       <source>Synchronize to:</source>
-      <translation type="unfinished">Synchronize to:</translation>
+      <translation>Synkronisér til:</translation>
     </message>
     <message>
       <location filename="../syncwindow.ui" line="144"/>
       <source>...</source>
-      <translation type="unfinished">...</translation>
+      <translation>...</translation>
     </message>
     <message>
       <location filename="../syncwindow.ui" line="183"/>
       <source>Start</source>
-      <translation type="unfinished">Start</translation>
+      <translation>Start</translation>
     </message>
     <message>
       <location filename="../syncwindow.ui" line="213"/>
       <source>Discovering Google Drive</source>
-      <translation type="unfinished">Discovering Google Drive</translation>
+      <translation>Opdag Google Drev</translation>
     </message>
     <message>
       <location filename="../syncwindow.ui" line="271"/>
       <source>Found 0 files</source>
-      <translation type="unfinished">Found 0 files</translation>
+      <translation>Fundet 0 filer</translation>
     </message>
     <message>
       <location filename="../syncwindow.ui" line="297"/>
       <source>Cancel</source>
-      <translation type="unfinished">Cancel</translation>
+      <translation>Annuller</translation>
     </message>
     <message>
       <location filename="../syncwindow.cpp" line="40"/>
       <source>Found %1 files</source>
-      <translation type="unfinished">Found %1 files</translation>
+      <translation>Fandt %1 filer</translation>
     </message>
     <message>
       <location filename="../syncwindow.cpp" line="50"/>
       <source>Select target directory</source>
-      <translation type="unfinished">Select target directory</translation>
+      <translation>Select target directory</translation>
     </message>
   </context>
 </TS>
