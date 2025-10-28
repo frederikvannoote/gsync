e496d7fea064818afe6aa721831e258bdf3b7fbb
Frederik Vannoote <frederikvannoote@users.noreply.github.com>
2025-10-28 17:05:35 +0100

    New translations gsync_en.ts (German)

--- i18n/GSync_de.ts
+++ i18n/GSync_de.ts
@@ -1,77 +1,77 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="de" sourcelanguage="en">
-<context>
+  <context>
     <name>SyncWindow</name>
     <message>
-        <location filename="../syncwindow.ui" line="14"/>
-        <source>Synchronize your Google Drive</source>
-        <translation>Synchronisieren Sie Ihr Google Drive</translation>
+      <location filename="../syncwindow.ui" line="14"/>
+      <source>Synchronize your Google Drive</source>
+      <translation>Synchronisieren Sie Ihr Google Drive</translation>
     </message>
     <message>
-        <location filename="../syncwindow.ui" line="43"/>
-        <source>Connect to your Google Drive</source>
-        <translation>Mit Ihrem Google Drive verbinden</translation>
+      <location filename="../syncwindow.ui" line="43"/>
+      <source>Connect to your Google Drive</source>
+      <translation>Mit Ihrem Google Drive verbinden</translation>
     </message>
     <message>
-        <location filename="../syncwindow.ui" line="66"/>
-        <source>Connect</source>
-        <translation>Verbinden</translation>
+      <location filename="../syncwindow.ui" line="66"/>
+      <source>Connect</source>
+      <translation>Verbinden</translation>
     </message>
     <message>
-        <location filename="../syncwindow.ui" line="89"/>
-        <source>Connected</source>
-        <translation>Verbunden</translation>
+      <location filename="../syncwindow.ui" line="89"/>
+      <source>Connected</source>
+      <translation>Verbunden</translation>
     </message>
     <message>
-        <location filename="../syncwindow.ui" line="131"/>
-        <source>Synchronize to:</source>
-        <translation>Synchronisieren mit:</translation>
+      <location filename="../syncwindow.ui" line="131"/>
+      <source>Synchronize to:</source>
+      <translation>Synchronisieren mit:</translation>
     </message>
     <message>
-        <location filename="../syncwindow.ui" line="144"/>
-        <source>...</source>
-        <translation>...</translation>
+      <location filename="../syncwindow.ui" line="144"/>
+      <source>...</source>
+      <translation>...</translation>
     </message>
     <message>
-        <location filename="../syncwindow.ui" line="183"/>
-        <source>Start</source>
-        <translation>Start</translation>
+      <location filename="../syncwindow.ui" line="183"/>
+      <source>Start</source>
+      <translation>Start</translation>
     </message>
     <message>
-        <location filename="../syncwindow.ui" line="213"/>
-        <source>Discovering Google Drive</source>
-        <translation>Google Drive entdecken</translation>
+      <location filename="../syncwindow.ui" line="213"/>
+      <source>Discovering Google Drive</source>
+      <translation>Google Drive entdecken</translation>
     </message>
     <message>
-        <location filename="../syncwindow.ui" line="271"/>
-        <source>Found 0 files</source>
-        <translation>0 Dateien gefunden</translation>
+      <location filename="../syncwindow.ui" line="271"/>
+      <source>Found 0 files</source>
+      <translation>0 Dateien gefunden</translation>
     </message>
     <message>
-        <location filename="../syncwindow.ui" line="297"/>
-        <source>Cancel</source>
-        <translation>Abbrechen</translation>
+      <location filename="../syncwindow.ui" line="297"/>
+      <source>Cancel</source>
+      <translation>Abbrechen</translation>
     </message>
     <message>
-        <location filename="../syncwindow.cpp" line="43"/>
-        <source>Running syncs: %1</source>
-        <translation type="unfinished"></translation>
+      <location filename="../syncwindow.cpp" line="43"/>
+      <source>Running syncs: %1</source>
+      <translation type="unfinished">Running syncs: %1</translation>
     </message>
     <message>
-        <location filename="../syncwindow.cpp" line="46"/>
-        <source>%1 files to analyze</source>
-        <translation type="unfinished"></translation>
+      <location filename="../syncwindow.cpp" line="46"/>
+      <source>%1 files to analyze</source>
+      <translation type="unfinished">%1 files to analyze</translation>
     </message>
     <message>
-        <location filename="../syncwindow.cpp" line="52"/>
-        <source>Found %1 files</source>
-        <translation>Gefunden %1 Dateien</translation>
+      <location filename="../syncwindow.cpp" line="52"/>
+      <source>Found %1 files</source>
+      <translation>Gefunden %1 Dateien</translation>
     </message>
     <message>
-        <location filename="../syncwindow.cpp" line="62"/>
-        <source>Select target directory</source>
-        <translation>Wählen Sie das Zielverzeichnis</translation>
+      <location filename="../syncwindow.cpp" line="62"/>
+      <source>Select target directory</source>
+      <translation>Wählen Sie das Zielverzeichnis</translation>
     </message>
-</context>
+  </context>
 </TS>
