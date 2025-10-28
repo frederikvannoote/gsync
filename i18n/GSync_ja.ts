721e8b3f84cc6feaf3f2af6879ba869614cee42d
Frederik Vannoote <frederikvannoote@users.noreply.github.com>
2025-10-28 17:05:40 +0100

    New translations gsync_en.ts (Japanese)

--- i18n/GSync_ja.ts
+++ i18n/GSync_ja.ts
@@ -1,77 +1,77 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="ja" sourcelanguage="en">
-<context>
+  <context>
     <name>SyncWindow</name>
     <message>
-        <location filename="../syncwindow.ui" line="14"/>
-        <source>Synchronize your Google Drive</source>
-        <translation>Google ドライブを同期</translation>
+      <location filename="../syncwindow.ui" line="14"/>
+      <source>Synchronize your Google Drive</source>
+      <translation>Google ドライブを同期</translation>
     </message>
     <message>
-        <location filename="../syncwindow.ui" line="43"/>
-        <source>Connect to your Google Drive</source>
-        <translation>Google ドライブに接続する</translation>
+      <location filename="../syncwindow.ui" line="43"/>
+      <source>Connect to your Google Drive</source>
+      <translation>Google ドライブに接続する</translation>
     </message>
     <message>
-        <location filename="../syncwindow.ui" line="66"/>
-        <source>Connect</source>
-        <translation>接続する</translation>
+      <location filename="../syncwindow.ui" line="66"/>
+      <source>Connect</source>
+      <translation>接続する</translation>
     </message>
     <message>
-        <location filename="../syncwindow.ui" line="89"/>
-        <source>Connected</source>
-        <translation>接続しました</translation>
+      <location filename="../syncwindow.ui" line="89"/>
+      <source>Connected</source>
+      <translation>接続しました</translation>
     </message>
     <message>
-        <location filename="../syncwindow.ui" line="131"/>
-        <source>Synchronize to:</source>
-        <translation>同期先:</translation>
+      <location filename="../syncwindow.ui" line="131"/>
+      <source>Synchronize to:</source>
+      <translation>同期先:</translation>
     </message>
     <message>
-        <location filename="../syncwindow.ui" line="144"/>
-        <source>...</source>
-        <translation>...</translation>
+      <location filename="../syncwindow.ui" line="144"/>
+      <source>...</source>
+      <translation>...</translation>
     </message>
     <message>
-        <location filename="../syncwindow.ui" line="183"/>
-        <source>Start</source>
-        <translation>開始</translation>
+      <location filename="../syncwindow.ui" line="183"/>
+      <source>Start</source>
+      <translation>開始</translation>
     </message>
     <message>
-        <location filename="../syncwindow.ui" line="213"/>
-        <source>Discovering Google Drive</source>
-        <translation>Google ドライブを発見中</translation>
+      <location filename="../syncwindow.ui" line="213"/>
+      <source>Discovering Google Drive</source>
+      <translation>Google ドライブを発見中</translation>
     </message>
     <message>
-        <location filename="../syncwindow.ui" line="271"/>
-        <source>Found 0 files</source>
-        <translation>0個のファイルが見つかりました</translation>
+      <location filename="../syncwindow.ui" line="271"/>
+      <source>Found 0 files</source>
+      <translation>0個のファイルが見つかりました</translation>
     </message>
     <message>
-        <location filename="../syncwindow.ui" line="297"/>
-        <source>Cancel</source>
-        <translation>キャンセル</translation>
+      <location filename="../syncwindow.ui" line="297"/>
+      <source>Cancel</source>
+      <translation>キャンセル</translation>
     </message>
     <message>
-        <location filename="../syncwindow.cpp" line="43"/>
-        <source>Running syncs: %1</source>
-        <translation type="unfinished"></translation>
+      <location filename="../syncwindow.cpp" line="43"/>
+      <source>Running syncs: %1</source>
+      <translation type="unfinished">Running syncs: %1</translation>
     </message>
     <message>
-        <location filename="../syncwindow.cpp" line="46"/>
-        <source>%1 files to analyze</source>
-        <translation type="unfinished"></translation>
+      <location filename="../syncwindow.cpp" line="46"/>
+      <source>%1 files to analyze</source>
+      <translation type="unfinished">%1 files to analyze</translation>
     </message>
     <message>
-        <location filename="../syncwindow.cpp" line="52"/>
-        <source>Found %1 files</source>
-        <translation>%1 ファイルが見つかりました</translation>
+      <location filename="../syncwindow.cpp" line="52"/>
+      <source>Found %1 files</source>
+      <translation>%1 ファイルが見つかりました</translation>
     </message>
     <message>
-        <location filename="../syncwindow.cpp" line="62"/>
-        <source>Select target directory</source>
-        <translation>ターゲットディレクトリを選択します。</translation>
+      <location filename="../syncwindow.cpp" line="62"/>
+      <source>Select target directory</source>
+      <translation>ターゲットディレクトリを選択します。</translation>
     </message>
-</context>
+  </context>
 </TS>
